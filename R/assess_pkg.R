b605442d85648c638cfa6161689cd700094c9fe6
GILLIAN-ext <u1004798@w-r-ide-prod-02.pharma.aventis.com>
2023-12-11 11:31:12 +0100

    add weights profile function

--- R/assess_pkg.R
+++ R/assess_pkg.R
@@ -108,24 +108,7 @@ assess_pkg <- function(
   
   #create sanofi weights profile
   
-  sanofi_weights <- c(
-    has_bug_reports_url = .2,
-    license = .1,
-    has_examples = .4,
-    has_maintainer = .2,
-    has_news = .2,
-    size_codebase = .2,
-    has_source_control = .1,
-    has_vignettes = .4,
-    has_website = .4,
-    news_current = .2,
-    export_help = .2,
-    export_calc = .2,
-    check = .75,
-    covr = .6,
-    dep_score = .5,
-    revdep_score = .1
-  )
+  sanofi_weights <- sanofi.risk.metric::create_weights_profile()
   
   results$overall_risk_score <- 
     sanofi.risk.metric::calc_overall_risk_score(results, 
