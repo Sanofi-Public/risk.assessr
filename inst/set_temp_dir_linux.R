8253f0503a0af72a4b474c36a8aa2f7f8723d59b
GILLIAN-ext <u1004798@w-r-ide-prod-02.pharma.aventis.com>
2024-04-23 13:00:05 +0200

    update set_temp_dir_linux script

--- inst/set_temp_dir_linux.R
+++ inst/set_temp_dir_linux.R
@@ -1,8 +1,33 @@
-# set tmp folder for installing packages locally
+# set tmp folder for installing packages locally in eWise
 
-Sys.setenv(R_SESSION_TMPDIR = "/home/u1004798/tmp")
+# get the user name
+user <- Sys.info()["user"]
+
+# create tmp dir based on user name
+tmp_dir <- paste0("/home/", user, "/tmp")
+
+# normalize the path
+tmp_dir <- normalizePath(tmp_dir)
+
+# check if the temp directory doesn't exist
+if (!dir.exists(tmp_dir)) {
+  message("temp directory exists: ", dir.exists(tmp_dir))
+  # create directory with all the files 
+  # in the current directory have all permissions type
+  dir.create(tmp_dir, showWarnings = TRUE, recursive = FALSE, mode = "0777")
+}
+
+# check directory existence
+message("temp directory exists: ", dir.exists(tmp_dir))
+
+# set R session temp directory
+Sys.setenv(R_SESSION_TMPDIR = tmp_dir)
 Sys.getenv("R_SESSION_TMPDIR") 
-# 
-unix:::set_tempdir("/home/u1004798/tmp")
-# [1] "/home/u1004798/tmp"
+
+# set Linux session temp directory
+unix:::set_tempdir(tmp_dir)
 tempdir()
+
+
+
+
