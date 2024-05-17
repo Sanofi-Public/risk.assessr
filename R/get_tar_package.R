5a57fa7268ada7a652429e7edc90c621699e0479
Bottois <Hugo.Bottois@sanofi.com>
2024-05-17 15:53:40 +0200

    function tar link + test

---
+++ R/get_tar_package.R
@@ -0,0 +1,50 @@
+library(testthat)
+library(httr2)
+library(dplyr)
+library(mockery)
+library(jsonlite)
+#' Get TAR Package URL
+#'
+#' This function retrieves the URL of a TAR package given its name and version.
+#'
+#' @param package_name A character string specifying the name of the package.
+#' @param version A character string specifying the version of the package.
+#'
+#' @return A list containing the response from the API, which includes the URL of the TAR package.
+#'
+#' @details This function sends a POST request to a specified API endpoint with the package name and version as the body. 
+#' If the request is successful, the function returns the response from the API, which contains the URL of the TAR package.
+#' If the request fails, the function stops and returns an error message with the status code.
+#'
+#' @import httr2
+#' @importFrom dplyr %>%
+#' @importFrom jsonlite fromJSON
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' get_tar_package("dplyr", "1.0.0")
+#' }
+get_tar_package <- function(package_name, version) {
+  url <- "https://r-connect-dev.prod.p488440267176.aws-emea.sanofi.com/content/2bb1fda0-b2fb-4686-b613-310d5a74e453/get_tar_link/"
+  body <- list(
+    name = package_name,
+    version = version
+  )
+  
+  # Perform the request
+  response <- request(url) %>%
+    req_body_json(body) %>%
+    req_headers(
+      accept = "application/json",
+      `Content-Type` = "application/json"
+    ) %>%
+    req_perform()
+  
+  # Check for a successful status code and print a success message
+  if (resp_status(response) != 200) {
+    stop("API request failed with status code: ", resp_status(response))
+  }
+  
+  return(resp_body_json(response))
+}
---
+++ tests/testthat/test-get_tar_package.R
@@ -0,0 +1,67 @@
+library(testthat)
+library(httr2)
+library(dplyr)
+library(mockery)
+library(jsonlite)
+
+
+test_that("test on correct result", {
+  # Mock function to simulate `req_perform`
+  mock_req_perform <- function(request) {
+    # Define the response body as a list
+    response_body <- 'http://example.com/package/ggplot2_3.3.5.tar.gz'
+    
+    # Convert the list to a JSON string
+    response_json <- toJSON(response_body, auto_unbox = TRUE)
+    
+    # Create the response object using httr2
+    base_response <- httr2::response(
+      status_code = 200,
+      url = "http://example.com",
+      headers = list("Content-Type" = "application/json"),
+      body = charToRaw(response_json)
+    )
+    
+    # Return the response object
+    return(base_response)
+  }
+  
+  # Mock the bindings
+  local_mocked_bindings(req_perform = mock_req_perform)
+  
+  # Test the result
+  result <- get_tar_package("ggplot2", "3.3.5")
+  expect_equal(result, "http://example.com/package/ggplot2_3.3.5.tar.gz")
+})
+
+
+
+test_that("test unsuccessful response", {
+  # Mock function to simulate `req_perform`
+  mock_req_perform <- function(request) {
+    # Create the response object with a 404 status code
+    base_response <- httr2::response(
+      status_code = 404,
+      url = "http://example.com",
+      headers = list("Content-Type" = "application/json"),
+      body = charToRaw('{"error": "Not Found"}')
+    )
+    
+    # Return the response object
+    return(base_response)
+  }
+  
+  # Mock the bindings
+  local_mocked_bindings(req_perform = mock_req_perform)
+  
+  # Test the result for an unsuccessful response
+  expect_error(get_tar_package("dummy_package", "3"),
+               "API request failed with status code: 404")
+})
+
+
+
+
+
+
+
