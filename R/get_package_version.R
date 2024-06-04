cc348aac62d1424c0498c9ddd0452075fe8208c9
Bottois <Hugo.Bottois@sanofi.com>
2024-06-04 18:46:16 +0200

    only comment

--- R/get_package_version.R
+++ R/get_package_version.R
@@ -20,17 +20,17 @@ library(lubridate)
 #' @importFrom httr2 request req_body_json req_headers req_perform resp_body_json resp_status
 #' @importFrom lubridate ymd_hm
 #' @examples
-#' Example usage:
-#' Assuming the function and the API endpoint are properly set up and accessible:
-#' sanofi.risk.metric::get_package_version("dplyr")
-#' Expected result (truncated):
-#' A tibble: 44 × 5
-#'    package_name    package_version link                 date             size 
-#'    <chr>           <chr>           <chr>                <chr>            <chr>
-#'  1 dplyr           0.1.1           dplyr_0.1.1.tar.gz   2014-01-29 21:24 530K 
-#'  2 dplyr           0.1.2           dplyr_0.1.2.tar.gz   2014-02-24 16:36 533K 
-#'  3 dplyr           0.1.3           dplyr_0.1.3.tar.gz   2014-03-15 00:36 535K 
-#'  4 dplyr           0.1             dplyr_0.1.tar.gz     2014-01-16 22:53 2.7M 
+#' # Example usage:
+#' # Assuming the function and the API endpoint are properly set up and accessible:
+#' # sanofi.risk.metric::get_package_version("dplyr")
+#' # Expected result (truncated):
+#' # A tibble: 44 × 5
+#' #   package_name    package_version link                 date             size 
+#' #  <chr>           <chr>           <chr>                <chr>            <chr>
+#' # 1 dplyr           0.1.1           dplyr_0.1.1.tar.gz   2014-01-29 21:24 530K 
+#' # 2 dplyr           0.1.2           dplyr_0.1.2.tar.gz   2014-02-24 16:36 533K 
+#' # 3 dplyr           0.1.3           dplyr_0.1.3.tar.gz   2014-03-15 00:36 535K 
+#' # 4 dplyr           0.1             dplyr_0.1.tar.gz     2014-01-16 22:53 2.7M 
 #' @export
 get_package_version <- function(package_name, last_num = 10) {
   url <- "https://r-connect-dev.prod.p488440267176.aws-emea.sanofi.com/content/2bb1fda0-b2fb-4686-b613-310d5a74e453/get_all_package_version/"
