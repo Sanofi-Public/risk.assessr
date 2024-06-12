ffd5d5bf76e77fa5335b0733e7c3196232dd7e1c
Bottois <Hugo.Bottois@sanofi.com>
2024-06-12 14:14:09 +0200

    bioconductor

---
+++ R/contains_vignette_folder.R
@@ -0,0 +1,64 @@
+library(utils)
+#' Check for Vignette Folder and .Rmd Files in a .tar File
+#'
+#' This function checks if a given .tar file contains a 'vignettes' folder
+#' and if there is at least one .Rmd file within that folder.
+#'
+#' @param tar_file A character string specifying the path to the .tar file to be checked.
+#' 
+#' @return A logical value: \code{TRUE} if the 'vignettes' folder exists and contains at least one .Rmd file,
+#' \code{FALSE} otherwise.
+#'
+#' @examples
+#' \dontrun{
+#'   tar_file <- "path/to/your/package.tar.gz"
+#'   result <- contains_vignette_folder(tar_file)
+#'   print(result)
+#' }
+#' 
+#' @export
+contains_vignette_folder <- function(tar_file) {
+  
+  if (!file.exists(tar_file)) {
+    stop("File does not exist. Please provide a valid .tar file.")
+  }
+
+  # Check if the file has a .tar extension
+  if (!grepl("\\.tar$", tar_file) && !grepl("\\.tar\\.gz$", tar_file) && !grepl("\\.tgz$", tar_file) &&
+      !grepl("\\.tar\\.bz2$", tar_file) && !grepl("\\.tbz2$", tar_file)) {
+    stop("Unsupported file type. Please provide a .tar file.")
+  }
+  
+  # Try to list the contents of the .tar file, handling any errors or warnings
+  file_list <- tryCatch(
+    {
+      utils::untar(tar_file, list = TRUE)
+    },
+    warning = function(w) {
+      warning("Warning in untar: ", conditionMessage(w))
+      return(character(0))
+    },
+    error = function(e) {
+      stop("Error in untar: ", conditionMessage(e))
+    }
+  )  
+  
+  # Normalize file paths to use forward slashes for consistency
+  normalized_paths <- gsub("\\\\", "/", file_list)
+  
+  # Extract paths that contain the vignettes folder
+  vignette_folder <- grep("/vignettes(/|$)", normalized_paths, value = TRUE)
+  
+  # Check if the vignettes folder exists
+  if (length(vignette_folder) == 0) {
+    return(FALSE)
+  }
+  
+  # Check for .Rmd files specifically within the vignette folder
+  vignette_rmd_files <- grep("/vignettes/[^/]+\\.Rmd$", normalized_paths, value = TRUE)
+  
+  # Determine if there are any .Rmd files in the vignette folder
+  has_rmd_in_vignette <- length(vignette_rmd_files) > 0
+  
+  return(has_rmd_in_vignette)
+}
