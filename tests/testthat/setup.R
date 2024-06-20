916d8a53f14b756361a4a0c8fbac928aa0b91ecc
GILLIAN-ext <u1004798@w-r-ide-prod-01.pharma.aventis.com>
2024-06-20 15:09:15 +0200

    test update_pscore_results code data

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -37,6 +37,43 @@ risk_results_1 <- list(
   revdep_score = .1260338
 )
 
+# create test data for testing update_pscore_results
+update_risk_results <- list(
+  pkg_name = "synapser",
+  pkg_version = "0.2.1",
+  pkg_source_path = "/tmp/RtmpNpDlUz/temp_file_1fe56774aacc/synapser",
+  has_bug_reports_url = "", 
+  has_examples = "",
+  has_maintainer = "",
+  size_codebase = "",
+  has_news = "",
+  has_source_control = "",
+  has_vignettes = "",
+  has_website = "",
+  news_current = "",
+  export_help = "",
+  export_calc = "",
+  check = "",
+  covr = "",
+  dep_score = "",
+  revdep_score = ""
+)
+
+# create test data for testing update_pscore_results
+update_pscore <- list(
+  export_help = 1,
+  has_bug_reports_url = NA,
+  size_codebase = .532,
+  has_maintainer = 1,
+  has_examples = 1,
+  has_news = 1,
+  has_source_control = 1,
+  has_vignettes = 1,
+  has_website = 1,
+  has_license = NA,
+  news_current = 1
+)
+
 # create overall risk scores to test risk profiles
 high_overall_risk_score <- .57
 medium_overall_risk_score <- .33
