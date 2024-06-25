4639f1e4d4f40b701ce28dbb0586aa67569cbc18
GILLIAN-ext <u1004798@w-r-ide-prod-01.pharma.aventis.com>
2024-06-25 10:57:35 +0200

    helper function test-run_coverage

--- tests/testthat/test-run_coverage.R
+++ tests/testthat/test-run_coverage.R
@@ -1,48 +1,57 @@
 
-test_that("Unpacking an empty tar file works correctly", {
+test_that("run test coverage for tar file works correctly", {
   
   dp <- system.file("testdata/diffdf-1.0.4.tar.gz", 
                      package = "sanofi.risk.metric")
-  pkg_disp <- stringr::str_extract(dp, "[^-]+")
+  pkg_disp <- stringr::str_extract(dp, "/testdata/[^-]+")
   
-  build_vignettes <- TRUE
+  # set up package
+  install_list <- set_up_pkg(dp, pkg_disp)
   
-  suppressWarnings(pkg_source_path <-  
-    sanofi.risk.metric::unpack_tarball(dp, pkg_disp))
-  
-  package_installed <- 
-    sanofi.risk.metric::install_package_local(pkg_source_path, 
-                                                      pkg_disp) 
-  
-  if (package_installed == TRUE ) {	
+  package_installed <- install_list$package_installed
+  pkg_source_path <- install_list$pkg_source_path
+  out_dir <- install_list$out_dir
+  results <- install_list$results
     
-    # get home directory
-    
-    pkg_desc <- get_pkg_desc(pkg_source_path, 
-                             fields = c("Package", 
-                                        "Version"))
-    pkg_name <- pkg_desc$Package
-    pkg_ver <- pkg_desc$Version
-   
-    out_dir <- "no audit trail"
-    
-    message("out_dir is ", out_dir)
-    
-    metadata <- sanofi.risk.metric::get_risk_metadata()
-    
-    results <- sanofi.risk.metric::create_empty_results(pkg_name,
-                                                        pkg_ver,
-                                                        pkg_source_path,
-                                                        metadata)
-    
+  if (package_installed == TRUE ) {
     covr_list <- run_coverage(pkg_source_path, out_dir) 
-  
+    
     # add total coverage to results
     results$covr <- covr_list$total_cov
     
     expect_true(checkmate::test_numeric(results$covr))
     
     testthat::expect_gt(results$covr, 0.1) 
-      
-  }
+  } else {
+    message(glue::glue("cannot run coverage test for {pkg_disp}"))
+  }    
+  
+})
+
+test_that("don't run test coverage for  empty tar file works correctly", {
+  
+  dp <- system.file("testdata/empty.tar.gz", 
+                    package = "sanofi.risk.metric")
+  pkg_disp <- stringr::str_extract(dp, "/testdata/[^-]+")
+  
+  # set up package
+  install_list <- set_up_pkg(dp, pkg_disp)
+  
+  package_installed <- install_list$package_installed
+  pkg_source_path <- install_list$pkg_source_path
+  out_dir <- install_list$out_dir
+  results <- install_list$results
+  
+  if (skip_if(package_installed != TRUE, 
+               message = glue::glue("cannot run coverage test for {pkg_disp}")) ) {
+    covr_list <- run_coverage(pkg_source_path, out_dir) 
+    
+    # add total coverage to results
+    results$covr <- covr_list$total_cov
+    
+    expect_true(checkmate::test_numeric(results$covr))
+    
+    testthat::expect_gt(results$covr, 0.1) 
+  } 
+  
 })
