2b766be55664ccfb829bac0cb9dc82562e308490
GILLIAN-ext <u1004798@w-r-ide-prod-01.pharma.aventis.com>
2024-06-25 12:28:41 +0200

    add test-parse_dcf_dependencies

---
+++ tests/testthat/test-parse_dcf_dependencies.R
@@ -0,0 +1,31 @@
+test_that("parse deps for tar file works correctly", {
+  
+  dp <- system.file("testdata/diffdf-1.0.4.tar.gz", 
+                    package = "sanofi.risk.metric")
+  pkg_disp <- stringr::str_extract(dp, "/testdata/[^-]+")
+  
+  r = getOption("repos")
+  r["CRAN"] = "http://cran.us.r-project.org"
+  options(repos = r)
+  
+  # set up package
+  install_list <- set_up_pkg(dp, pkg_disp)
+  
+  package_installed <- install_list$package_installed
+  pkg_source_path <- install_list$pkg_source_path
+
+  if (package_installed == TRUE ) {	
+    deps <- sanofi.risk.metric::parse_dcf_dependencies(pkg_source_path)
+    
+    expect_identical(length(deps), 2L)
+    
+    expect_true(checkmate::check_data_frame(deps, 
+                                            any.missing = FALSE))
+    
+    expect_true(checkmate::check_data_frame(deps, 
+                                            col.names = "named"
+    )
+    )
+  }
+  
+})
