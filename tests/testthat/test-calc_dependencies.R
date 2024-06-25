8c57b733a9ff1fc2fbef2a9e8aded17e209f8738
GILLIAN-ext <u1004798@w-r-ide-prod-01.pharma.aventis.com>
2024-06-25 12:28:09 +0200

    add test-calc_dependencies

---
+++ tests/testthat/test-calc_dependencies.R
@@ -0,0 +1,34 @@
+test_that("parse deps for tar file works correctly", {
+  
+  r = getOption("repos")
+  r["CRAN"] = "http://cran.us.r-project.org"
+  options(repos = r)
+  
+  dp <- system.file("testdata/diffdf-1.0.4.tar.gz", 
+                    package = "sanofi.risk.metric")
+  pkg_disp <- stringr::str_extract(dp, "/testdata/[^-]+")
+  
+  # set up package
+  install_list <- set_up_pkg(dp, pkg_disp)
+  
+  package_installed <- install_list$package_installed
+  pkg_source_path <- install_list$pkg_source_path
+
+  if (package_installed == TRUE ) {	
+    deps_list <- sanofi.risk.metric::calc_dependencies(pkg_source_path)
+    
+    expect_identical(length(deps_list), 2L)
+    
+    expect_true(checkmate::check_data_frame(deps_list$deps, 
+                                            any.missing = FALSE)
+                )
+    
+    expect_true(checkmate::check_data_frame(deps_list$deps, 
+                                            col.names = "named"
+                                            )
+               )
+    
+    testthat::expect_gt(deps_list$dep_score, 0.010) 
+  }
+  
+})
