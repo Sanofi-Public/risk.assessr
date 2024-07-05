c1ee154692f2521977207c496c08d81c89c34704
GILLIAN-ext <u1004798@w-r-ide-prod-02.pharma.aventis.com>
2024-07-05 16:47:41 +0200

    update setup helpers

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -120,3 +120,21 @@ test_deps <- data.frame(package, type)
 high_overall_risk_score <- .57
 medium_overall_risk_score <- .33
 low_overall_risk_score <- .24
+
+### Set up packages ###
+
+# Build all packages
+pkg_dirs <- setup_multiple_pkgs()
+
+# select specific packages for scoring (other types may only be used in `test-results` or other tests)
+# only need a subset of these for majority of tests
+pkg_select <- pkg_dirs$pkg_setups_df %>%
+  dplyr::filter(pkg_type %in% c(
+    "pass_success", "pass_no_docs", "pass_no_functions",
+    "fail_func_syntax", "fail_test"
+  ))
+pkg_tars <- pkg_select %>% dplyr::pull(tar_file)
+names(pkg_tars) <- pkg_select$pkg_type
+
+# run at the end of all tests
+withr::defer(unlink(pkg_dirs$testing_dir, recursive = TRUE), teardown_env())
