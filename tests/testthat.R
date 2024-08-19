6dccd21a9f6d3b6587124dc0b5e1ea0403ede770
GILLIAN-ext <u1004798@w-r-ide-prod-02.pharma.aventis.com>
2024-08-19 10:32:16 +0200

    comment out tests where test files not found

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,9 @@
+# setting R_TESTS to empty string because of
+# https://github.com/hadley/testthat/issues/144
+# and https://github.com/r-lib/testthat/issues/86
+# revert this when that issue in R is fixed.
+Sys.setenv("R_TESTS" = "")
+
 library(testthat)
 library(sanofi.risk.metric)
 
