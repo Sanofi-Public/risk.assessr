9747f345bad45faaa09bc760945c96cb7bbed727
Bottois <Hugo.Bottois@sanofi.com>
2024-09-10 17:16:46 +0200

    remove params

--- R/modify_description_file.R
+++ R/modify_description_file.R
@@ -24,7 +24,8 @@
 modify_description_file <- function(tar_file) {
   # Create a temporary directory
   temp_dir <- tempdir()
-  
+
+  # Extract the tar file into the temporary directory
   tryCatch({
     suppressWarnings({
       untar(tar_file, exdir = temp_dir)
@@ -33,54 +34,39 @@ modify_description_file <- function(tar_file) {
     stop("Error in untarring the file: ", e$message)
   })
   
-  # Automatically find the package directory by assuming it's the only subfolder in temp_dir
+  # Automatically find the package directory by looking for the DESCRIPTION file
   subdirs <- list.dirs(temp_dir, full.names = TRUE, recursive = FALSE)
   
-  if (length(subdirs) == 0) {
-    stop("No subdirectories found in the extracted tar file.")
-  }
-  
-  # Print subdirs for debugging purposes
-  print(subdirs)
-  
-  # Automatically find the package directory by looking for the DESCRIPTION file
-  subdirs <- list.dirs(temp_dir)
-  
-  # Search for the directory containing the DESCRIPTION file
   package_dir <- NULL
+  package_name <- NULL
   for (dir in subdirs) {
     potential_description_file <- file.path(dir, "DESCRIPTION")
     if (file.exists(potential_description_file)) {
       package_dir <- dir
-      print("selected")
-      print(package_dir)
+      package_name <- basename(dir)
       break
     }
   }
   
-  #package_dir <- subdirs[1]
-  
-  # Check if the package directory was found
   if (is.null(package_dir)) {
     stop("Package directory not found (no DESCRIPTION file found).")
   }
   
   description_file <- file.path(package_dir, "DESCRIPTION")
-  
   if (!file.exists(description_file)) {
     stop("DESCRIPTION file not found in the extracted package directory.")
   }
   
   # Modify the DESCRIPTION file
   description_content <- readLines(description_file)
-  # Check if the line already exists
+  
   if ("Config/build/clean-inst-doc: FALSE" %in% description_content) {
     return(tar_file)
   }
   
   description_content <- c(description_content, "Config/build/clean-inst-doc: FALSE")
   writeLines(description_content, description_file)
-  
+
   # Recreate a new tar.gz file
   modified_tar_file <- tempfile(fileext = ".tar.gz")
   
@@ -89,13 +75,18 @@ modify_description_file <- function(tar_file) {
   
   tryCatch({
     suppressWarnings({
-      tar(modified_tar_file, files = basename(package_dir), compression = "gzip", tar = "internal")
+      tar(modified_tar_file, files = package_name, compression = "gzip", tar = "internal")
     })
   }, error = function(e) {
-    setwd(current_wd)  # Ensure we return to the original directory on error
+    setwd(current_wd) 
     stop("Error in creating the tar.gz file: ", e$message)
   })
+  
   setwd(current_wd)
+  
+  # # Clean up extracted files in temp_dir
+  # unlink(package_dir)
+  # unlink(package_dir, recursive = TRUE)
   
   return(modified_tar_file)
 }
